07e73d5a88a4cc80297d36b952bdce6fd9635ea3
Vinay Selvaraj <vinay@selvaraj.com>
2020-01-21 16:54:20 -0500

    Added step to download artifact

--- xcode_builder/builder.py
+++ xcode_builder/builder.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import boto3
+import tempfile
+import zipfile
 
 cp = boto3.client('codepipeline')
 
@@ -8,18 +10,37 @@ def handle_job(job):
 
     # Acknowledge the job
     cp.acknowledge_job(
-        jobId = job['id'],
-        nonce = job['nonce']
+       jobId = job['id'],
+       nonce = job['nonce']
     )
 
     # Configure S3 with the artifact credentials
     s3 = boto3.client(
         's3',
-        aws_access_key_id=job['data']['artifactCredentials']['accessKeyId']
+        aws_access_key_id=job['data']['artifactCredentials']['accessKeyId'],
         aws_secret_access_key=job['data']['artifactCredentials']['secretAccessKey'],
         aws_session_token=job['data']['artifactCredentials']['sessionToken']
     )
 
+    # Download the input artifact
+    input_artifact_s3_location = job['data']['inputArtifacts'][0]['location']['s3Location']
+    _, input_artifact = tempfile.mkstemp()
+    s3.download_file(input_artifact_s3_location['bucketName'], input_artifact_s3_location['objectKey'], input_artifact)
+    print(input_artifact)
+
+    # Exctract the input artifact
+    build_dir = tempfile.mkdtemp()
+    with zipfile.ZipFile(input_artifact, 'r') as zr:
+        zr.extractall(build_dir)
+    print(build_dir)
+
+    cp.put_job_failure_result(
+        jobId = job['id'],
+        failureDetails={
+            'type' : 'JobFailed',
+            'message' : 'BUILD FAILED'
+        }
+    )
     
 
 
@@ -38,6 +59,7 @@ def main():
         )
 
         for job in poll_result['jobs']:
+            print(job)
             handle_job(job)
         
         break
